a8a54f72a14d55a025c2a7d058791c6c0a73dbb8
osama-alawneh <h.a.alawneh9@gmail.com>
2021-05-06 22:33:18 +0300

    complete the first part of toDoList Project

--- app.js
+++ app.js
@@ -2,43 +2,34 @@ const express= require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+let items = ["Buy Food" , "Cook Food" , "Eat food"];
+
 app.set('view engine' , 'ejs');
+
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/' , (req,res) =>{
    const today = new Date();
-   const currentDay = today.getDay();
    let day = '';
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-    }
     const options = {
-        kindOfDay : day,
-        yourName : "Osama"
-    }
-    res.render("list" , options);
+        weekday : "long",
+        day : "numeric",
+        month : "long"
+    };
+
+    day = today.toLocaleDateString("en-US" , options);
+
+    res.render("list" , {kindOfDay : day , newListItems : items});
 });
 
+app.post('/' , (req,res) =>{
+    let item = req.body.toDoInput;
+    if (item !== '') {
+        items.push(item);
+    }
+    res.redirect('/');
+});
 
 
 //Port Settings
